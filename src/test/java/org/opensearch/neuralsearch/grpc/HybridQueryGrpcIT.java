ce16f36cdab945396901c3af33a243cab665cd78
Medha Srivatsa <msriniva@us.ibm.com>
2026-04-14 16:16:22 -0700

    Ignore gRPC test case until implemented in jvector
    Signed-off-by: Medha Srivatsa <msriniva@us.ibm.com>

--- src/test/java/org/opensearch/neuralsearch/grpc/HybridQueryGrpcIT.java
+++ src/test/java/org/opensearch/neuralsearch/grpc/HybridQueryGrpcIT.java
@@ -18,7 +18,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
+import org.apache.lucene.tests.util.LuceneTestCase;
 import org.opensearch.client.Request;
 import org.opensearch.client.Response;
 import org.opensearch.neuralsearch.BaseNeuralSearchIT;
@@ -349,7 +349,7 @@ public class HybridQueryGrpcIT extends BaseNeuralSearchIT {
      * Test hybrid query with lexical (Match) + neural/vector (KNN) sub-queries.
      * This demonstrates combining traditional text search with vector similarity search.
      */
-    @Ignore("JVector plugin does not include gRPC KNN query converter")
+    @LuceneTestCase.AwaitsFix(bugUrl = "https://github.com/opensearch-project/opensearch-jvector/issues/391")
     @SneakyThrows
     public void testHybridQueryWithLexicalAndKnnSubQueries() {
         // Query vector close to doc1's embedding [0.1, 0.2, 0.3]
@@ -370,7 +370,7 @@ public class HybridQueryGrpcIT extends BaseNeuralSearchIT {
     /**
      * Test hybrid query with lexical + KNN + filter to demonstrate full hybrid search capability.
      */
-    @Ignore("JVector plugin does not include gRPC KNN query converter")
+    @LuceneTestCase.AwaitsFix(bugUrl = "https://github.com/opensearch-project/opensearch-jvector/issues/391")
     @SneakyThrows
     public void testHybridQueryWithLexicalKnnAndFilter() {
         float[] queryVector = new float[] { 0.4f, 0.5f, 0.6f };
